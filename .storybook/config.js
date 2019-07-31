665f7964cbb7518f1ab1131888fc2d60edac5d63
Michèle Legait <michele@travelaer.com>
2019-07-31 08:20:35 +0200

    Organize stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,13 @@
-import { load } from "@storybook/react";
+import { addParameters, configure } from "@storybook/react";
 
-// automatically import all files ending in *.stories.js or *.stories.mdx
-load(require.context("../src", true, /\.stories\.(js|mdx)$/), module);
+addParameters({
+  options: {
+    storySort: (a, b) => a[1].id.localeCompare(b[1].id)
+  }
+});
+
+// automatically import all files ending in *.stories.js or *.stories.mdx or *.stories.ts or *.stories.tsx
+configure(
+  require.context("../src/stories", true, /\.stories\.(js|ts|tsx|mdx)$/),
+  module
+);

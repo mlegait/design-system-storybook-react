b5de2acb732ceff3870fd19fd741009912f0848a
Michèle Legait <michele@travelaer.com>
2019-08-06 12:34:18 +0200

    Sort stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,19 @@ import { addParameters, configure } from "@storybook/react";
 
 addParameters({
   options: {
-    storySort: (a, b) => a[1].id.localeCompare(b[1].id)
+    storySort: (a, b) => {
+      // Sort stories per position. If there is no position or if the position is the same,
+      // sort stories alphabetically.
+      const aPosition = a[1].parameters.position;
+      const bPosition = b[1].parameters.position;
+      if (aPosition && bPosition) {
+        const comparision = aPosition - bPosition;
+        if (comparision !== 0) {
+          return comparision;
+        }
+      }
+      return a[0].localeCompare(b[0]);
+    }
   }
 });
 
---
+++ src/stories/Components/Checkbox.stories.js
@@ -0,0 +1,8 @@
+import { storiesOf } from "@storybook/react";
+import React from "react";
+
+storiesOf("Components|Checkbox", module).add(
+  "default",
+  () => <div>To replace with a checkbox</div>,
+  { position: 40 }
+);

da365c04444b8bf291f8ec637044034e5954338b
Michèle Legait <michele@travelaer.com>
2019-08-23 14:15:19 +0200

    Try to use withKnobs

--- src/stories/Components/Link.stories.js
+++ src/stories/Components/Link.stories.js
@@ -1,9 +1,11 @@
+import { text, withKnobs } from '@storybook/addon-knobs';
 import React from 'react';
 import Link from '../../components/Link/Link';
 
 export default {
   title: 'Components|Navigation/Link',
   component: Link,
+  decorators: [withKnobs],
   parameters: {
     componentSubtitle:
       'Links are used primarily as a navigational element. Links may also change what or how data is displayed (i.e., view more, show all). If the action taken by the user will change or manipulate data, use a button.',
@@ -11,5 +13,7 @@ export default {
 };
 
 export const link = () => (
-  <Link href="https://storybook.js.org/">Storybook official website</Link>
+  <Link href={text('href', 'https://storybook.js.org/')}>
+    Storybook official website
+  </Link>
 );

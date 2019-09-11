249673ed89d6dbeddb56c6b0663e06949e20c5a7
Michèle Legait <michele@travelaer.com>
2019-09-11 13:06:20 +0200

    New "Get started / Design" doc page

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,11 +6,16 @@ addDecorator(story => <Container story={story} />);
 
 // We want to force the files to load in a specific order.
 const loaderFn = () => [
+  require('../src/DesignSystem/GetStarted/Design.stories.mdx'),
+
   require('../src/DesignSystem/Guidelines/Content.stories.mdx'),
+
   require('../src/DesignSystem/Components/Accordion.stories'),
   require('../src/DesignSystem/Components/Select.stories'),
   require('../src/DesignSystem/Components/SkeletonText.stories'),
+
   require('../src/stories/Components/Button.stories.mdx'),
+
   require('../src/DesignSystem/Resources/DesignResources.stories.mdx'),
 ];
 

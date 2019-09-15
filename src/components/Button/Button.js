134b00dda66ae67a751c0fb1bf377ac7c1839d04
Michèle Legait <michele@travelaer.com>
2019-09-15 08:27:59 +0200

    Improve Button description

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -15,27 +15,21 @@ const { prefix } = settings;
 
 /**
  * Buttons are used to initialize an action, either in the background or
-  foreground of an experience.
-
-  There are several kinds of buttons.
-
-  Primary buttons should be used for the principle call to action
-  on the page.
-
-  Secondary buttons should be used for secondary actions on each page.
-
-  Danger buttons should be used for a negative action (such as Delete) on the page.
-
-  Modify the behavior of the button by changing its event properties.
-
-  Field buttons may be use directly next to an input element, to visually align their heights.
-
-  Small buttons may be used when there is not enough space for a
-  regular sized button. This issue is most found in tables. Small button should have three words
-  or less.
-
-  When words are not enough, icons can be used in buttons to better communicate what the button does. Icons are
-  always paired with text.
+ * foreground of an experience.
+ *
+ * There are several kinds of buttons.
+ * - **Primary** buttons should be used for the principle call to action on the page.
+ * - **Secondary** buttons should be used for secondary actions on each page.
+ * - **Danger** buttons should be used for a negative action (such as Delete) on the page.
+ * - **Field** buttons may be use directly next to an input element, to visually align their heights.
+ * - **Small** buttons may be used when there is not enough space for a
+ * regular sized button. This issue is most found in tables. Small button should have three words
+ * or less.
+ *
+ * When words are not enough, **icons** can be used in buttons to better communicate what the button does. Icons are
+ * always paired with text.
+ *
+ * Modify the behavior of the button by changing its event properties.
  */
 const Button = React.forwardRef(function Button(
   {

ba5a04c4d856ce2547a2ad655408bf197afb1dc7
Michele Legait <michele.legait+github@gmail.com>
2020-01-30 09:36:37 +0100

    Fix story description

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -18,11 +18,13 @@ const { prefix } = settings;
  * foreground of an experience.
  *
  * There are several kinds of buttons.
+ *
  * - **Primary** buttons should be used for the principle call to action on the page.
  * - **Secondary** buttons should be used for secondary actions on each page.
  * - **Danger** buttons should be used for a negative action (such as Delete) on the page.
  * - **Field** buttons may be use directly next to an input element, to visually align their heights.
  * - **Small** buttons may be used when there is not enough space for a
+ *
  * regular sized button. This issue is most found in tables. Small button should have three words
  * or less.
  *
